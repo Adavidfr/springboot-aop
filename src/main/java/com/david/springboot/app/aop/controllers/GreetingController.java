faadb74b6b76001fae8c8be679377f1b6ff2f213
Alex David Frías <adavidfruiz@gmail.com>
2024-05-14 15:02:00 -0500

    feat: add greeting service

--- src/main/java/com/david/springboot/app/aop/controllers/GreetingController.java
+++ src/main/java/com/david/springboot/app/aop/controllers/GreetingController.java
@@ -1,5 +1,7 @@
 package com.david.springboot.app.aop.controllers;
 
+import com.david.springboot.app.aop.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,11 @@ import java.util.Collections;
 @RestController
 public class GreetingController {
 
+    @Autowired
+    private GreetingService greetingService;
+
     @GetMapping("/greeting")
     public ResponseEntity<?> greeting() {
-        return ResponseEntity.ok(Collections.singletonMap("greeting", null));
+        return ResponseEntity.ok(Collections.singletonMap("greeting", greetingService.sayHello("Pepe", "Hola que tal!")));
     }
 }
