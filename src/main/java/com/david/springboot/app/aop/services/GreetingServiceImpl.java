8249bc61db15043bd337895f461b9a6bac962eee
Alex David Frías <adavidfruiz@gmail.com>
2024-05-14 15:02:24 -0500

    feat: add greeting service implementation

---
+++ src/main/java/com/david/springboot/app/aop/services/GreetingServiceImpl.java
@@ -0,0 +1,14 @@
+package com.david.springboot.app.aop.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingServiceImpl implements GreetingService {
+
+    @Override
+    public String sayHello(String person, String phrase) {
+        String greeting = phrase + " " + person;
+        System.out.println(greeting);
+        return greeting;
+    }
+}
